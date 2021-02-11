2b4318ac6b23f9a004992372f602d29140189c3f
cowdingus <zaki.nadhif45@gmail.com>
2021-02-11 17:37:44 +0700

    Wrote LuaScriptComponent interface

--- include/Engine/Components/LuaScriptComponent.hpp
+++ include/Engine/Components/LuaScriptComponent.hpp
@@ -1,7 +1,21 @@
 #pragma once
 
+#include <string>
+#include <string_view>
+
 struct LuaScriptComponent
 {
 public:
-	
+	LuaScriptComponent(std::string_view sourceCode);
+
+	bool isValid() const;
+
+	void onAttach();
+	void onDetach();
+
+	void fixedUpdate(float deltaTime);
+	void update(float deltaTime);
+
+private:
+	std::string_view m_sourceCode;
 };
