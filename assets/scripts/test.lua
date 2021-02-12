0fe320acd4413bdef8cf8aceb502e394805e97b9
cowdingus <zaki.nadhif45@gmail.com>
2021-02-12 18:09:20 +0700

    Wrote test.lua

---
+++ assets/scripts/test.lua
@@ -0,0 +1,20 @@
+local function onAttach()
+
+end
+
+local function onDetach()
+
+end
+
+local function handleEvent(event)
+
+end
+
+local function update(dt)
+
+end
+
+local function fixedUpdate(dt)
+	
+end
+	
