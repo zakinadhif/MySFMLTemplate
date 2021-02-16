5fa641d7b34ba4df7b96cc556997343b7d8332f0
cowdingus <56494343+cowdingus@users.noreply.github.com>
2021-02-16 07:25:36 +0700

    ScriptResource interface changes.  - No copy constructor  - Doesn't store source code  - Stores compiled function

--- include/Engine/Scripting/ScriptResource.hpp
+++ include/Engine/Scripting/ScriptResource.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <sol/state_view.hpp>
+#include <sol/protected_function_result.hpp>
+
 #include <string>
 #include <string_view>
 
@@ -9,20 +12,25 @@ namespace zfge
 class ScriptResource
 {
 public:
-	ScriptResource();
-	ScriptResource(const ScriptResource& other);
+	ScriptResource(sol::state_view lua);
+	ScriptResource(const ScriptResource& copy) = delete;
 	ScriptResource(ScriptResource&& other) noexcept;
 
 	bool loadFromFile(const std::string& path);
 	bool reload();
 
-	bool isEmpty() const;
+	bool isValid() const;
 	
-	const std::string& getSourceCode() const;
+	sol::protected_function getCompiledFunction();
 
 private:
+	void loadScript(std::string_view sourceCode);
+
+	sol::state_view m_lua;
+
 	std::string m_path;
-	std::string m_sourceCode;
+
+	sol::load_result m_result;
 };
 
 }
--- src/Engine/Scripting/ScriptResource.cpp
+++ src/Engine/Scripting/ScriptResource.cpp
@@ -1,38 +1,54 @@
 #include "Engine/Scripting/ScriptResource.hpp"
 
 #include <fstream>
+#include <spdlog/spdlog.h>
 
 namespace zfge
 {
 
-ScriptResource::ScriptResource()
+ScriptResource::ScriptResource(sol::state_view lua)
+	: m_lua(lua)
 {
-
+	
 }
 
-ScriptResource::ScriptResource(const ScriptResource& copy)
-	: m_path(copy.m_path)
-	, m_sourceCode(copy.m_sourceCode)
+ScriptResource::ScriptResource(ScriptResource&& other) noexcept
+	: m_lua(other.m_lua)
+	, m_path(std::move(other.m_path))
+	, m_result(std::move(other.m_result))
 {
-
 }
 
 bool ScriptResource::loadFromFile(const std::string& path)
 {
+	std::string sourceCode;
+
 	try
 	{
-		std::ifstream ifs(path);
-		std::string newSourceCode{std::istreambuf_iterator<char>(ifs), std::istreambuf_iterator<char>()};
-		
-		m_sourceCode.swap(newSourceCode);
-		m_path = path;
+		std::ifstream scriptFile(path);
+		sourceCode = {std::istreambuf_iterator<char>(scriptFile), std::istreambuf_iterator<char>()};
 	}
-	catch (std::exception e)
+	catch (...)
 	{
 		return false;
 	}
 
+	m_path = path;
+	loadScript(sourceCode);
+
 	return true;
+}
+
+void ScriptResource::loadScript(std::string_view sourceCode)
+{
+	m_result = m_lua.load(sourceCode);
+
+	if (!m_result.valid())
+	{
+		sol::error err = m_result;
+
+		SPDLOG_WARN("ScriptResource: Loaded invalid script at {}. Error: {}", m_path, err.what());
+	}
 }
 
 bool ScriptResource::reload()
@@ -40,14 +56,14 @@ bool ScriptResource::reload()
 	return loadFromFile(m_path);
 }
 
-bool ScriptResource::isEmpty() const
+bool ScriptResource::isValid() const
 {
-	return m_sourceCode.empty();
+	return m_result.valid();
 }
 
-const std::string& ScriptResource::getSourceCode() const
+sol::protected_function ScriptResource::getCompiledFunction()
 {
-	return m_sourceCode;
+	return m_result;
 }
 
 }
